fa8571cc0cb1a14a63be762fec0924301a58275d
Darzamat12 <heavylesha@gmail.com>
2019-11-25 10:13:30 +0300

    feat fixed bags, add saving to local storage

--- palette/main.js
+++ palette/main.js
@@ -1,10 +1,11 @@
-let canvas=document.getElementById("canvas");
-
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext('2d');
 let currentTool="pencil";
-let currentColor="#FFEB3B";
-let previusColor="#000000";
+let currentColor=localStorage.getItem("currentColor");
+let previusColor=localStorage.getItem("previusColor");
 
-let tools=["pencil", "bucket","colorPicker"];
+changeColor();
+
 let basicColors=[
 ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"],
 ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
@@ -12,43 +13,46 @@ let basicColors=[
 ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"]
 ];
 
-
+if(localStorage.getItem("canvas")){
+    var dataURL = localStorage.getItem("canvas");
+    var img = new Image;
+    img.src = dataURL;
+    img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+};
+}
+else{
 for (let i=0;i<4;i++)
     for(let j=0;j<4;j++){
-canvas.getContext('2d').fillStyle=basicColors[i][j];
-canvas.getContext('2d').fillRect(128 * i, 128 * j, 128, 128);
+ctx.fillStyle=basicColors[i][j];
+ctx.fillRect(128 * i, 128 * j, 128, 128);
     }
+}
 canvas.onmousedown=function(event){
     if(currentTool==="pencil"){
-        canvas.getContext('2d').fillStyle=currentColor;
-        console.log(event.offsetX);
-        canvas.getContext('2d').fillRect(128*Math.floor(event.offsetX/128), 128*Math.floor(event.offsetY/128),128,128);
-
+        ctx.fillStyle=currentColor;
+        ctx.fillRect(128*Math.floor(event.offsetX/128), 128*Math.floor(event.offsetY/128),128,128);
         canvas.onmousemove=function(event){
-            canvas.getContext('2d').fillRect(128*Math.floor(event.offsetX/128), 128*Math.floor(event.offsetY/128),128,128);
+            ctx.fillRect(128*Math.floor(event.offsetX/128), 128*Math.floor(event.offsetY/128),128,128);
         }
-
         canvas.onmouseup=()=>{
             canvas.onmousemove=null;
+            localStorage.setItem("canvas", canvas.toDataURL());
         }
     }
-    else if(currentTool == "bucket"){
-
-        canvas.getContext('2d').fillStyle = currentColor;
-        canvas.getContext('2d').fillRect(0, 0, 512, 512);
+    else if(currentTool === "bucket"){
+        ctx.fillStyle = currentColor;
+        ctx.fillRect(0, 0, 512, 512);
     }
 
     else if(currentTool==="colorPicker"){
-        let r = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data[0];
-        let g = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data[1];
-        let b = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data[2];
+        let r = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[0];
+        let g = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[1];
+        let b = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[2];
         previusColor=currentColor;
-        currentColor=rgbTranslate(r,g,b);
-        changeColor();
-        
+        currentColor=rgbTranslate(r,g,b);    
     }
 
-    localStorage.setItem("canvas", canvas);
 }
 
 document.addEventListener("keyup",function(event){
@@ -66,6 +70,7 @@ document.addEventListener("keyup",function(event){
         document.querySelector(".tools__color-picker").classList.add("active-tool");
         break;
     }
+    
 })
 
 function rgbTranslate(r,g,b) {
@@ -73,9 +78,9 @@ function rgbTranslate(r,g,b) {
     g = g.toString(16);
     b = b.toString(16);
   
-    if (r.length == 1) r = "0" + r;
-    if (g.length == 1) g = "0" + g;
-    if (b.length == 1) b = "0" + b;
+    if (r.length === 1) r = "0" + r;
+    if (g.length === 1) g = "0" + g;
+    if (b.length === 1) b = "0" + b;
   
     return "#" + r + g + b;
 }
@@ -84,41 +89,25 @@ function rgbTranslate(r,g,b) {
 function changeColor(){
     document.querySelector('.previus-circle').style.backgroundColor=previusColor;
     document.querySelector('.current-circle').style.backgroundColor=currentColor;
+    
 }
 
 
-function changeCurrentColor(event){
 
-    if(event.target.children[0] == document.querySelector(".colors__previus-color")){
-        let temp = currentColor;
-        currentColor = previousColor;
-        previousColor = temp;
-        changeColors();
-    }
-
-    if(event.target.children[0] == document.querySelector(".colors__red")){
-        previousColor = currentColor;
-        currentColor = "#FF0000";
-        changeColors();
-    }
-
-    if(event.target.children[0] == document.querySelector(".colors__blue")){
-        previousColor = currentColor;
-        currentColor = "#0000FF";
-        changeColors();
-    }
-
-}
 
 document.querySelector(".colors__red").addEventListener("click",function(event){
     previusColor=currentColor;
     currentColor="#FF0000";
+    localStorage.setItem("currentColor",currentColor);
+    localStorage.setItem("previusColor", previusColor);
     changeColor();
 })
 
 document.querySelector(".colors__blue").addEventListener("click",function(event){
     previusColor=currentColor;
     currentColor="#0000FF";
+    localStorage.setItem("currentColor",currentColor);
+    localStorage.setItem("previusColor", previusColor);
     changeColor();
 })
 
@@ -126,6 +115,8 @@ document.querySelector(".colors__previus-color").addEventListener("click",functi
     let tmp=previusColor;
     previusColor=currentColor;
     currentColor=tmp;
+    localStorage.setItem("currentColor",currentColor);
+    localStorage.setItem("previusColor", previusColor);
     changeColor();
 })
 
